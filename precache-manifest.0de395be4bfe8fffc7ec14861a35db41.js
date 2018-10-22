53d2536c818e8c79c0e7eb30baf67b3411b7e7f9
Vikram Mullick <vikram1.mullick@gmail.com>
2018-10-22 03:08:38 -0700

    Updates

---
+++ precache-manifest.0de395be4bfe8fffc7ec14861a35db41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab1de659e47268ba2df1",
+    "url": "/drf-coding-challenge/static/js/runtime~main.ab1de659.js"
+  },
+  {
+    "revision": "fe2f415a3fe168e8114c",
+    "url": "/drf-coding-challenge/static/js/main.fe2f415a.chunk.js"
+  },
+  {
+    "revision": "791d1d8f2413407f96db",
+    "url": "/drf-coding-challenge/static/js/1.791d1d8f.chunk.js"
+  },
+  {
+    "revision": "e7a2bd3d91059f6eba52f0771b6863ca",
+    "url": "/drf-coding-challenge/index.html"
+  }
+];
